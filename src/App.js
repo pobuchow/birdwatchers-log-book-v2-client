19eaa6b43d15e8dd52befd9f8b3fa3eef83837a6
pobuchow <pobu1@hotmail.com>
2020-05-03 20:19:30 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Birdwatchers Log Book - app for birdwatchers</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
