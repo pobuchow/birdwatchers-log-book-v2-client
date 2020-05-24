125bbe12b3dc97135c861b869cd2287b8f06830a
pobuchow <pobu1@hotmail.com>
2020-05-24 20:51:38 +0200

    some styling and basic login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,58 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h3>React Login Component</h3>
-            </div>
-        );
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& label.Mui-focused': {
+            color: '#595142',
+          },
+        '& .MuiFilledInput-underline:after': {
+            borderBottomColor: '#595142',
+        },
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+            background: "#BFAE56",
+        },
+    },
+    button: {
+        color: '#595142',
+        backgroundColor: "#A69677",
+        "&:hover": {
+          backgroundColor: "#A69677"
+        }
+      },
+}));
+
+export default function Login() {
+    const classes = useStyles();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = () => {
+        alert(`user: ${username} pass:  ${password}`);
     }
+    
+    return (
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={ handleSubmit }>
+            <TextField
+                required
+                id="login-username"
+                label="username"
+                variant="filled"
+                onChange = {(_e) => setUsername(_e.target.value)}
+            />
+            <TextField
+                required
+                id="login-password"
+                label="password"
+                type="password"
+                autoComplete="current-password"
+                variant="filled"
+                onChange = {(_e) => setPassword(_e.target.value)}
+            />
+            < Button variant="contained" className={classes.button} type="submit">Login</Button>
+        </form>
+    );
 }
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,20 +7,22 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 import Login from './../login/Login';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1, 
+  },
+  toolbar:{
+    background: "#3726A6"
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
+    color: "#ACC3F2"
   },
 }));
 
@@ -31,14 +33,11 @@ export default function NavBar(props) {
     <Router>
       <div className={classes.root}>
         <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
+          <Toolbar className={classes.toolbar}>
               <Typography  variant="h6" className={classes.title} data-testid="navbar-title" color="inherit" style={{textDecoration: 'none'}} component={Link} to={"/"}>
                 {props.title}
               </Typography>
-              <Button data-testid="navbar-login-button" color="inherit" style={{textDecoration: 'none'}} component={Link} to={"/login"}>Login</Button>
+              <Button data-testid="navbar-login-button" style={{textDecoration: 'none', color: "#ACC3F2"}} component={Link} to={"/login"}>Login</Button>
           </Toolbar>
         </AppBar>
       </div>
